dbf6a5bdb78c92e1bdb5f90a044cd3f6cc7fdda0
stephanie <stephaniechiamaka63@gmail.com>
2022-07-26 03:04:10 +0100

    second commit

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ let num1 = parseFloat(prompt("Number 1: "));
 let num2 = parseFloat(prompt("Number 2: "));
 
 if(opr == "+") {
-    console.log (num1 + num2);
+    alert (num1 + num2);
 } else if(opr == "-") {
-    console.log (num1 - num2);
+    alert (num1 - num2);
 } else if(opr == "/") {
-    console.log (num1 / num2);
+    alert (num1 / num2);
 } else if(opr == "*") {
-    console.log (num1 * num2);
+    alert (num1 * num2);
 }
